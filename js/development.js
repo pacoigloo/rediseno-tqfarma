8e19ab31362228821d6402ca6a051c437bee4b88
pacoigloo <felipe.zapata@igloo-lab.com.co>
2015-05-07 20:25:34 +0000

    Version 1.0.14

--- js/development.js
+++ js/development.js
@@ -1,8 +1,10 @@
 var activeMenu = false;
+var urls = [];
 
 $(document).ready(function () {
     var offsetMenuTop = $(".section_menu").offset().top;
     positionLogin();
+    linksMobile();
     $('#initSession').unbind().click(function () {
         if ($('#popUpInicio').is(':visible') && window.innerWidth >= 768) {
             $('#popUpInicio').fadeOut(500);
@@ -16,7 +18,6 @@ $(document).ready(function () {
     $(".section_menu > ul > li:first-child").unbind().click(function () {
         if(window.innerWidth < 768){
             activeMenu = !activeMenu;
-            console.log("ActiveMenu: "+ activeMenu);
             $(".section_menu > ul > li:not('.section_menu > ul > li:first-child')").slideToggle('1000');
         }
     });
@@ -28,6 +29,7 @@ $(document).ready(function () {
         var offsetMenuTop = $(".section_menu").offset().top;
         positionLogin();
         topMenuChilds();
+        linksMobile();
     });
     
     $(window).load(function(){
@@ -82,3 +84,31 @@ $(".firstLevel > li > a").click(function(){
         $(this).parent().children("ul.secondLevel").slideDown(500);
     }
 });
+
+function linksMobile(){
+    //Verifica tamaño de window
+    if(window.innerWidth >= 768){
+        //Si es mayor que 768 y el array de urls no esta vacio devuelve el valor de los atributos href a cada elemento
+        if(urls.length != 0){
+            $.each(urls, function( i, v ) {
+              if(i != "undefined"){
+                  $(".firstLevel > li:nth-of-type("+i+") > a").attr("href",v);
+              }
+            });
+            urls = [];
+        }
+    }else if(window.innerWidth < 768){
+        //Si es mayor que 768 y el array de urls esta vacio elimina el atributo href de cada elemento y lo guarda en urls
+        if(urls.length == 0){
+            //Cuenta desde el inicio hasta la cantidad de li's
+            for(var i = 1;i < $(".firstLevel > li").size()+1;i++){
+                //Comprueba si li tiene ul
+                if($(".firstLevel > li:nth-of-type("+i+")").has("ul").size() != 0){
+                    //Si tiene ul entonces le saca el href y lo quita
+                    urls[ i] = $(".firstLevel > li:nth-of-type("+i+") > a").attr("href");
+                    $(".firstLevel > li:nth-of-type("+i+") > a").removeAttr("href");
+                }
+            }
+        }
+    }
+}
