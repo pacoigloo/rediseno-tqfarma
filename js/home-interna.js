8126aabd5fed0709c8bf88ebf3b86c795bc89129
pacoigloo <felipe.zapata@igloo-lab.com.co>
2015-05-27 03:57:09 +0000

    Version 1.0.16

---
+++ js/home-interna.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    var offsetMenuTop = $(".section_menu").offset().top;
+    $('#initSession').unbind().click(function () {
+        if ($('#popUpInicio').is(':visible') && $(window).width() >= 768) {
+            $('#popUpInicio').fadeOut(500);
+        } else if ($(window).width() >= 768){
+            $('#popUpInicio').fadeIn(500);
+        }
+    });
+    $(".section_menu > ul > li:first-child").unbind().click(function(){
+        $(".section_menu ul li:not('.section_menu > ul > li:first-child')").slideToggle('1000');
+        
+    });
+    
+    mostrarLineas();
+    
+    $(window).scroll(function(){
+        comprobarDistTop(offsetMenuTop);
+    });
+    $(window).resize(function() {
+        var offsetMenuTop = $(".section_menu").offset().top;
+        mostrarLineas();
+    });
+
+});
+
+function comprobarDistTop(offsetMenuTop){
+    if($(window).scrollTop() > offsetMenuTop){
+        $(".section_menu").addClass("menu_fixed");
+    }else if($(window).scrollTop() < offsetMenuTop){
+        $(".section_menu").removeClass("menu_fixed");
+    }
+}
+
+$(function() {
+	$( '#dl-menu' ).dlmenu({
+		animationClasses : { classin : 'dl-animate-in-5', classout : 'dl-animate-out-5' }
+	});
+});
+
+
+function mostrarLineas(){
+    if(window.innerWidth >= 768)
+    {
+        $('.columna-central #webTemp').css('display','block');
+        $('.columna-central #movilTemp').css('display','none');
+    }
+    else
+    {
+        if(window.innerWidth < 768)
+        {
+            $('.columna-central #movilTemp').css('display','block');
+            $('.columna-central #webTemp').css('display','none');
+            
+        }
+    }
+    
+}
